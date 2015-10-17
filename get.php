95f7d529446b282d593852684ca1b708ca6bb9d8
DhruvGala <dhruv.g123@gmail.com>
2015-10-17 07:46:52 -0400

    getting comments for each idea_initial version

---
+++ get.php
@@ -0,0 +1,37 @@
+<html>
+<body>
+<?php
+
+$conn = mysql_connect("localhost", "root", "");
+
+if(!$conn){
+	die("connection failed: " . mysql_error());
+}
+
+mysql_select_db("ChildAid");
+
+if(isset($_POST['idea'])){
+	$idea = $_POST['idea'];
+
+	$sql = "SELECT comment from comments where idea_ID = (SELECT idea_ID from idea where description = '$idea')";
+
+	$retval = mysql_query($sql, $conn);
+
+	if(!$retval){
+		echo 'Could not fetch data: ' . mysql_error();
+	}
+
+	while($row = mysql_fetch_array($retval)){
+		echo "{$row['comment']}<br>";
+	}
+}
+else{
+	echo "could not fetch data";
+}
+
+?>
+
+</body>
+
+
+</html>
