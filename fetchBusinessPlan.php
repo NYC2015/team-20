b99588d10dc1f1577f68063ff374c95eb06aa796
DhruvGala <dhruv.g123@gmail.com>
2015-10-17 11:00:38 -0400

    developed the business logic

--- fetchBusinessPlan.php
+++ fetchBusinessPlan.php
@@ -21,17 +21,38 @@ mysql_select_db($dbname);
 
 function fetch_data(){
 	if($_POST["plan"] || $_POST["product"] || $_POST["marketing"]
-		$_POST["statup_cost"] || $_POST["cost_per_unit"] || 
-		$_POST["revenue"]){
-		echo
-	$inserquery = INSERT INTO `childaid_cfg2015`.`business_plan` 
+		$_POST["statup_cost"] || $_POST["target_revenue"]){
+		
+		$target_revenue = $_POST["target_revenue"];
+		$cost = $_POST["startup_cost"];
+
+		$margin = ($target_revenue - $cost)/100;
+
+
+		$inserquery = INSERT INTO `childaid_cfg2015`.`business_plan` 
 		('idea_ID','plan','product desceiption','marketing plan',
-			'startup_cost','cost_per_unit','revenue') VALUES ($idea_ID,);
+			'startup_cost','cost_per_unit','revenue','product_sold','target_revenue') 
+		VALUES 
+		($idea_ID, $_POST["plan"],$_POST["product"],$_POST["marketing"],
+			$_POST["startup_cost"],$margin,NULL,NULL,$target_revenue);
 	}
 
 	mysql_query(inserquery);
 
 	echo("You are all set, now track your progress");
+}
+
+function compute_profit(){
+	if($_POST["product_sold"] || $_POST["cost_per_unit"]){
+		$revenue = $_POST["product_sold"] * $_POST["cost_per_unit"];
+
+		$inserquery = INSERT INTO 'childaid_cfg2015'.'business_plan'
+		('idea_ID','plan','product desceiption','marketing plan',
+			'startup_cost','cost_per_unit','revenue','product_sold','target_revenue') 
+		VALUES 
+		($idea_ID, $_POST["plan"],$_POST["product"],$_POST["marketing"],
+			$_POST["startup_cost"],$margin,$revenue,$_POST["product_sold"],$target_revenue);
+	}
 }
 
 fetch_data();
